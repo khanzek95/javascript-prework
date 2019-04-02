240a98df99433d465c591ae2e604988474a555f3
Kacper Hanżek <hanzyk1995@gmail.com>
2019-04-02 20:46:45 +0200

    v6 working

--- js/script.js
+++ js/script.js
@@ -14,9 +14,9 @@ function buttonClicked(buttonName) {
    displayResult(playerMove, computerMove);
 }
 
-buttonRock = document.getElementById('button-rock').name;
-buttonPaper = document.getElementById('button-paper').name;
-buttonScissors = document.getElementById('button-scissors').name;
+buttonRock = document.getElementById('button-rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
