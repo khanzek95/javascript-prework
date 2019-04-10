4f5a0ad14eded90c64e028c5aaddcb981cf28a3f
Kacper Hanżek <hanzyk1995@gmail.com>
2019-04-10 12:25:19 +0200

    Repair Code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-var ButtonName, buttonTest;
 
-/**
- * Describe this function...
- */
 function buttonClicked(buttonName) {
   clearMessages();
   console.log(buttonName + ' został kliknięty');
@@ -14,82 +10,46 @@ function buttonClicked(buttonName) {
    displayResult(playerMove, computerMove);
 }
 
+function getMoveName(MoveId) {
+  console.log('wywołano funkcję getMoveName z argumentem: ' + MoveId);
+  if (MoveId == 1) {
+    return 'kamień';
+  } 
+  else if (MoveId == 2) {
+	  return 'papier';
+  }
+else if (MoveId == 3) {
+	   return 'nożyce';
+} 
+   else {
+    printMessage('Nie znam ruchu o id ' + MoveId + '. Zakładam, że chodziło o "kamień,papier lub nożyce".');
+    return 'kamień,papier czy no';
+  }
+}
+
+function displayResult(PlayerMove, ComputerMove) {
+  console.log('wywołano funkcję displayResults z argumentami: ' + PlayerMove + ', ' + ComputerMove);
+  if (PlayerMove == 'papier' && ComputerMove == 'kamień') {
+    printMessage('Wygrywasz!');
+  }
+  else if (PlayerMove == 'kamień' && ComputerMove == 'nożyce') {
+	  printMessage('Wygrywasz!');
+  }
+  else if (PlayerMove == 'nożyce' && ComputerMove == 'papier') {
+	  printMessage('Wygrywasz!');
+  }
+  else if (PlayerMove == ComputerMove) {
+	  printMessage('Remis!');
+  }
+  else {
+    printMessage('Przegrywasz :(');
+  }
+  printMessage('Zagrałem ' + ComputerMove + ', a Ty ' + PlayerMove);
+}
+
 buttonRock = document.getElementById('button-rock');
 buttonPaper = document.getElementById('button-paper');
 buttonScissors = document.getElementById('button-scissors');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
-
-
-var playerMove;
-playerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nozyczki, to wygrywasz!');
-playerMove = 'nożyce';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
-
-
-var playerMove, playerInput;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-} else if (playerInput == '3') {
-  playerMove = 'nożyce';
-} else {
-  playerMove = 'nieznany ruch';
-}
-printMessage('Twój ruch: ' + playerMove);
-
-playerInput == '1';
-
-var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-
-/**
- * Describe this function...
- */
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-    return 'kamień';
-  } 
-  else if (argMoveId == 2) {
-	  return 'papier';
-  }
-else if (argMoveId == 3) {
-	   return 'nożyce';
-} 
-   else {
-    printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień,papier lub nożyce".');
-    return 'kamień,papier czy no';
-  }
-}
-
-/**
- * Describe this function...
- */
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('Wygrywasz!');
-  }
-  else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-	  printMessage('Wygrywasz!');
-  }
-  else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-	  printMessage('Wygrywasz!');
-  }
-  else if (argPlayerMove == argComputerMove) {
-	  printMessage('Remis!');
-  }
-  else {
-    printMessage('Przegrywasz :(');
-  }
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-}
-
-
